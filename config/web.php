9665773a60042b8c00df6f327e18a86bb2f7a6b5
Daxiong\da xiong <daxionginfo@gmail.com>
2016-12-05 08:29:39 +0700

    fixed

--- config/web.php
+++ config/web.php
@@ -7,8 +7,9 @@ $config = [
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'components' => [
+        'backuprestore' => ['class' => '\oe\modules\backuprestore\Module',], //'layout' => '@admin-
         'request' => [
-            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+// !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 's1-5KKKVQEHaMrpC9XaCSSWxpV8ilnac',
         ],
         'cache' => [
@@ -28,8 +29,8 @@ $config = [
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
             // send all mails to a file by default. You have to set
-            // 'useFileTransport' to false and configure a transport
-            // for the mailer to send real emails.
+// 'useFileTransport' to false and configure a transport
+// for the mailer to send real emails.
             'useFileTransport' => true,
         ],
         'log' => [
